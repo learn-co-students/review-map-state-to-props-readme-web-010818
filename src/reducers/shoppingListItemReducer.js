62a8c4884d2a85651518996ce8bd6351bc576332
feihafferkamp <fei.hafferkamp@gmail.com>
2018-04-03 15:11:35 -0400

    Done.

--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -1,21 +1,18 @@
-export default function shoppingListItemReducer(state = {
-  items: [], 
-  users: [ 'initial user' ]
-}, action) {
+const defaultState = { items: [], users: ['initial user'] };
 
-  switch(action.type) {
+export default function shoppingListItemReducer(state = defaultState, action) {
+	switch (action.type) {
+		case 'GET_COUNT_OF_ITEMS':
+			return Object.assign({}, state, {
+				items: state.items.concat(state.items.length + 1)
+			});
 
-    case 'GET_COUNT_OF_ITEMS':
-      return Object.assign({}, state, {
-        items: state.items.concat(state.items.length + 1)
-      });
+		case 'GET_COUNT_OF_USERS':
+			return Object.assign({}, state, {
+				users: state.users.concat(state.users.length + 1)
+			});
 
-    case 'GET_COUNT_OF_USERS':
-      return Object.assign({}, state, {
-        users: state.users.concat(state.users.length + 1)
-      });
-
-    default:
-      return state;
-  }
-};
+		default:
+			return state;
+	}
+}
